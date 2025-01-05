364682b41be9a217ab3a66bcd93d1533a6dafac9
tuansdf <103062682+tuansdf@users.noreply.github.com>
2025-01-05 17:13:56 +0700

    add excel cell styles

--- src/main/java/org/tuanna/xcloneserver/modules/report/TestUserImportTemplate.java
+++ src/main/java/org/tuanna/xcloneserver/modules/report/TestUserImportTemplate.java
@@ -11,33 +11,30 @@ import java.util.function.Function;
 @Slf4j
 public class TestUserImportTemplate implements ImportTemplate<TestUser> {
 
-    private static final List<String> header = List.of("ID", "Username", "Email", "Name", "Address", "Street", "City", "Country", "Created At", "Updated At");
-    private static final Function<List<String>, TestUser> rowExtractor = row -> {
+    private static final List<String> HEADER = List.of("ID", "Username", "Email", "Name", "Address", "Street", "City", "Country", "Created At", "Updated At");
+    private static final Function<List<String>, TestUser> ROW_EXTRACTOR = row -> {
         TestUser result = new TestUser();
-        try {
-            result.setId(UUIDUtils.fromString(row.get(0)));
-            result.setUsername(row.get(1));
-            result.setEmail(row.get(2));
-            result.setName(row.get(3));
-            result.setAddress(row.get(4));
-            result.setStreet(row.get(5));
-            result.setCity(row.get(6));
-            result.setCountry(row.get(7));
-            result.setCreatedAt(DateUtils.toZonedDateTime(row.get(8), DateUtils.Formatter.DATE_TIME_BE));
-            result.setUpdatedAt(DateUtils.toZonedDateTime(row.get(9), DateUtils.Formatter.DATE_TIME_BE));
-        } catch (Exception ignored) {
-        }
+        result.setId(UUIDUtils.fromString(row.get(0)));
+        result.setUsername(row.get(1));
+        result.setEmail(row.get(2));
+        result.setName(row.get(3));
+        result.setAddress(row.get(4));
+        result.setStreet(row.get(5));
+        result.setCity(row.get(6));
+        result.setCountry(row.get(7));
+        result.setCreatedAt(DateUtils.toZonedDateTime(row.get(8), DateUtils.Formatter.DATE_TIME_BE));
+        result.setUpdatedAt(DateUtils.toZonedDateTime(row.get(9), DateUtils.Formatter.DATE_TIME_BE));
         return result;
     };
 
     @Override
     public List<String> getHeader() {
-        return header;
+        return HEADER;
     }
 
     @Override
     public Function<List<String>, TestUser> getRowExtractor() {
-        return rowExtractor;
+        return ROW_EXTRACTOR;
     }
 
 }
