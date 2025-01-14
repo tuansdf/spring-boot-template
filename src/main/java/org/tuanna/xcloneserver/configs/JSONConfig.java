53a04ea21e96013983815f1397f043a685945ef5
tuansdf <103062682+tuansdf@users.noreply.github.com>
2025-01-14 11:13:47 +0700

    update

--- src/main/java/org/tuanna/xcloneserver/configs/JSONConfig.java
+++ src/main/java/org/tuanna/xcloneserver/configs/JSONConfig.java
@@ -1,21 +1,16 @@
 package org.tuanna.xcloneserver.configs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.tuanna.xcloneserver.utils.JSONUtils;
 
 @Configuration
 public class JSONConfig {
 
     @Bean
     public ObjectMapper getObjectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-        return objectMapper;
+        return JSONUtils.getObjectMapper();
     }
 
 }
