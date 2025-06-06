ec61962e6610838e46a34853e4b82ef54db80c1f
tuansdf <103062682+tuansdf@users.noreply.github.com>
2025-06-06 23:21:09 +0700

    locale

--- src/main/java/com/example/sbt/common/util/LocaleHelper.java
+++ src/main/java/com/example/sbt/common/util/LocaleHelper.java
@@ -29,8 +29,8 @@ public class LocaleHelper {
         try {
             if (ArrayUtils.isNotEmpty(args)) {
                 for (int i = 0; i < args.length; i++) {
-                    if (args[i] != null && args[i] instanceof LocaleKey v && v.arg != null) {
-                        args[i] = getMessage(v.arg, locale);
+                    if (args[i] != null && args[i] instanceof LocaleKey(String arg) && arg != null) {
+                        args[i] = getMessage(arg, locale);
                     }
                 }
             }
