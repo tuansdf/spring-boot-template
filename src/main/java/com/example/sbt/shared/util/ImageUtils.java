c96fe1621e89ba87140f08d57aa222129673344f
tuansdf <103062682+tuansdf@users.noreply.github.com>
2025-07-02 18:37:53 +0700

    image

--- src/main/java/com/example/sbt/shared/util/ImageUtils.java
+++ src/main/java/com/example/sbt/shared/util/ImageUtils.java
@@ -3,42 +3,86 @@ package com.example.sbt.shared.util;
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import net.coobird.thumbnailator.Thumbnails;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Iterator;
 
 @Slf4j
 public class ImageUtils {
 
     public static void compressImage(InputStream input, OutputStream output, Options options) throws IOException {
         if (input == null || output == null) return;
-        BufferedImage image = ImageIO.read(input);
-        var builder = Thumbnails.of(image);
-        if (options.getWidth() != null && image.getWidth() > options.getWidth()) {
-            builder = builder.width(options.getWidth());
-        } else {
-            builder = builder.width(image.getWidth());
+        ImageWriter writer = null;
+        ImageOutputStream ios = null;
+        try {
+            BufferedImage originalImage = ImageIO.read(input);
+            if (originalImage == null) {
+                throw new IOException("Could not decode image from input stream");
+            }
+
+            BufferedImage imageToWrite = originalImage;
+
+            if (options.getWidth() != null && options.getHeight() != null) {
+                Image scaled = originalImage.getScaledInstance(
+                        options.getWidth(),
+                        options.getHeight(),
+                        Image.SCALE_SMOOTH
+                );
+                BufferedImage resized = new BufferedImage(
+                        options.getWidth(),
+                        options.getHeight(),
+                        originalImage.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : originalImage.getType()
+                );
+                Graphics2D g2d = resized.createGraphics();
+                g2d.drawImage(scaled, 0, 0, null);
+                g2d.dispose();
+                imageToWrite = resized;
+            }
+
+            // Determine format
+            String format = options.getFormat() != null ? options.getFormat() : "jpg";
+
+            // Get writer
+            Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName(format);
+            if (!writers.hasNext()) {
+                throw new IllegalArgumentException("No ImageWriter found for format: " + format);
+            }
+            writer = writers.next();
+
+            ios = ImageIO.createImageOutputStream(output);
+            writer.setOutput(ios);
+
+            ImageWriteParam param = writer.getDefaultWriteParam();
+
+            // Set compression quality for JPEG
+            if (param.canWriteCompressed() && options.getQuality() != null) {
+                param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+                param.setCompressionQuality(options.getQuality());
+            }
+
+            writer.write(null, new IIOImage(imageToWrite, null, null), param);
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.dispose();
+                }
+                if (ios != null) {
+                    ios.flush();
+                }
+            } catch (Exception ignored) {
+            }
         }
-        if (options.getHeight() != null && image.getHeight() > options.getHeight()) {
-            builder = builder.height(options.getHeight());
-        } else {
-            builder = builder.height(image.getHeight());
-        }
-        if (options.getQuality() != null) {
-            builder = builder.outputQuality(options.getQuality());
-        }
-        if (StringUtils.isNotBlank(options.getFormat())) {
-            builder = builder.outputFormat(options.getFormat());
-        } else {
-            builder = builder.useOriginalFormat();
-        }
-        builder.toOutputStream(output);
     }
 
     public static byte[] compressImageToBytes(MultipartFile file, Options options) {
