3321a3583970ee30b1930e764e8040865a518020
tuansdf <103062682+tuansdf@users.noreply.github.com>
2025-09-21 09:48:05 +0700

    remove base64

--- src/main/java/com/example/sbt/common/util/CompressUtils.java
+++ src/main/java/com/example/sbt/common/util/CompressUtils.java
@@ -5,23 +5,19 @@ import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
 import org.apache.commons.compress.compressors.gzip.GzipParameters;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.util.zip.Deflater;
 
 @Slf4j
 public class CompressUtils {
-    public static byte[] toGzip(byte[] bytes) {
-        try {
-            GzipParameters parameters = new GzipParameters();
-            parameters.setCompressionLevel(Deflater.BEST_SPEED);
-            try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                 OutputStream outputStream = new GzipCompressorOutputStream(byteArrayOutputStream, parameters)) {
-                outputStream.write(bytes);
-                return byteArrayOutputStream.toByteArray();
-            }
-        } catch (Exception e) {
-            log.error("toGzip {}", e.toString());
-            return null;
+    public static byte[] toGzip(byte[] bytes) throws IOException {
+        GzipParameters parameters = new GzipParameters();
+        parameters.setCompressionLevel(Deflater.BEST_SPEED);
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+             OutputStream outputStream = new GzipCompressorOutputStream(byteArrayOutputStream, parameters)) {
+            outputStream.write(bytes);
+            return byteArrayOutputStream.toByteArray();
         }
     }
 }
--- src/main/java/com/example/sbt/infrastructure/config/JSONConfig.java
+++ src/main/java/com/example/sbt/infrastructure/config/JSONConfig.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.commons.lang3.RandomUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
